24c36dbb666d9f0ee2fea042528bdab5bd8b71d9
Danny <d.berkelaar1@student.avans.nl>
2020-06-03 15:24:08 +0200

    Created dishes crud

---
+++ resources/views/admin/dishes/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <a href="{{ route('admin.dishes.create') }}">Voeg menu item toe</a>
+        
+        <table>
+            <tr>
+                <th>Nummer</th>
+                <th>Naam</th>
+                <th></th>
+            </tr>
+        
+            @foreach ($menuitems as $item)
+                <tr>
+                    <td>{{ $item->menunumber }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>
+                        <a href="{{route('admin.dishes.edit', ['menuitem' => $item->id])}}">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,18 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/kassa', 'CashDeskController@index')->name('cashdesk');
 Route::get('/kassa/gerechten', 'DishesController@index')->name('dishes');
 Route::get('/kassa/sales', 'SalesoverviewController@index')->name('salesoverview');
-Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/admin', 'Admin\DishesController@index')->name('admin.dishes');
+Route::get('/admin/dishes/create', 'Admin\DishesController@create')->name('admin.dishes.create');
+Route::post('/admin/dishes', 'Admin\DishesController@store')->name('admin.dishes.store');
+Route::get('/admin/dishes/{menuitem}/edit', 'Admin\DishesController@edit')->name('admin.dishes.edit');
+Route::put('/admin/dishes/{menuitem}', 'Admin\DishesController@update')->name('admin.dishes.update');
+Route::delete('/admin/dishes/{menuitem}', 'Admin\DishesController@destroy')->name('admin.dishes.delete');
 
 Route::get('/contact', function() {
     return view('contact');
